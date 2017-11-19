efcc1e16a3af26b61cfc8cbff57a9032c0b1057d
bobimel2nd <31457563+bobimel2nd@users.noreply.github.com>
2017-11-18 22:00:27 -0800

    Version 1.0

---
+++ assets/js/crystal.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+	var RndNumber = 0;
+	var SumNumber = 0;
+	var Wins = 0;
+	var Losses = 0;
+
+	$(".crystalPic").on("click", function() {
+		SumNumber += parseInt($(this).attr("val"));
+	    UpdateDisplay();
+		if (SumNumber < RndNumber) {
+			return;
+		} else if (SumNumber > RndNumber) {
+			Losses++;
+		} else {
+			Wins++;
+		}
+		StartGame();
+	});
+
+	StartGame();
+	
+	function StartGame() {
+		RndNumber = PickNumber(19,120);
+		SumNumber = 0;
+		$(".crystalPic").each(function(){
+	        $(this).attr("val",PickNumber(1,12));
+	    });
+	    UpdateDisplay();
+	};
+
+	function UpdateDisplay() {
+		$("#RndNumber").text("Add up to this Number: " + RndNumber.toString());
+		$("#SumNumber").text("Your Total so Far: " + SumNumber.toString());
+		$("#Wins").text("Wins: " + Wins.toString());
+		$("#Losses").text("Losses: " + Losses.toString());
+	}
+
+	function PickNumber(Low, High) {
+		return Math.floor(Math.random()*(High-Low+1) + Low)
+	};
+})
